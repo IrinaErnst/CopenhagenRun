75264ead8da0fa01adbef2a79f722288205b5c1f
IrinaKalashnikova@gmail.com <IrinaKalashnikova@gmail.com>
2017-07-15 21:50:31 -0400

    Add progress ring and start button

--- CopenhagenRun/ViewController.swift
+++ CopenhagenRun/ViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import UICircularProgressRing
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICircularProgressRingDelegate {
 
+    @IBOutlet weak var progressRing: UICircularProgressRingView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        progressRing.animationStyle = kCAMediaTimingFunctionLinear
+        @IBAction func startButtonPressed(_ sender: Any) {
+        }
+        progressRing.font = UIFont.systemFont(ofSize: 70)
+        progressRing.maxValue = 50
+        progressRing.innerRingColor = UIColor.blue
+        
+        progressRing.setProgress(value: 30, animationDuration: 10.0){
+            print("Done animating")
+        }
+        
+        progressRing.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // The delegate method!
+    func finishedUpdatingProgress(forRing ring: UICircularProgressRingView) {
+        if ring === ring1 {
+            print("From delegate: Ring 1 finished")
+        } else if ring === ring2 {
+            print("From delegate: Ring 2 finished")
+        }
     }
 
 
