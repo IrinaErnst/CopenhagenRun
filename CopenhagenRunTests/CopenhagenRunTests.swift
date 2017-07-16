869e2e142a317a231a5c7f616b1e7963d0f292ab
IrinaKalashnikova@gmail.com <IrinaKalashnikova@gmail.com>
2017-07-15 20:49:35 -0400

    First commit

---
+++ CopenhagenRunTests/CopenhagenRunTests.swift
@@ -0,0 +1,36 @@
+//
+//  CopenhagenRunTests.swift
+//  CopenhagenRunTests
+//
+//  Created by Irina Ernst on 7/15/17.
+//  Copyright © 2017 IrinaErnst. All rights reserved.
+//
+
+import XCTest
+@testable import CopenhagenRun
+
+class CopenhagenRunTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
