9b0c52fe67eb5e10cf1d45b488bfec8987802a56
IrinaErnst <IrinaKalashnikova@gmail.com>
2017-10-03 15:09:20 -0400

    Change status bar color to light color

---
+++ CopenhagenRun/ViewControllers/LoginViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  CopenhagenRun
+//
+//  Created by Irina Ernst on 10/3/17.
+//  Copyright © 2017 IrinaErnst. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
