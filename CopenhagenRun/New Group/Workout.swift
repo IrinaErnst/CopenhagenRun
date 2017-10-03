dd1434678b136af0da2314f97733682d5ad87b56
Slobodan Kovrlija <slobkov02@yahoo.com>
2017-10-03 15:21:15 -0400

    Modify model properties

--- CopenhagenRun/New Group/Workout.swift
+++ CopenhagenRun/New Group/Workout.swift
@@ -12,15 +12,15 @@ import RealmSwift
 // Workout model
 final class Workout: Object {
     @objc dynamic var id = 0
-    let numberOfCycles = RealmOptional<Int>()
-    let totalTimeMinutes = RealmOptional<Int>()
-    let date = RealmOptional<Int>()
+    @objc dynamic var numberOfCycles = 0
+    @objc dynamic var totalTimeMinutes = 0
+    @objc dynamic var date = Date()
     let calories = RealmOptional<Int>()
     let averageSpeed = RealmOptional<Int>()
     let maxSpeed = RealmOptional<Int>()
     let totalDistance = RealmOptional<Int>()
     let numberOfSteps = RealmOptional<Int>()
-    let rating = RealmOptional<Int>()
+    @objc dynamic var rating: String? = nil
     
     let user = LinkingObjects(fromType: User.self, property: "workouts")
     @objc dynamic var userId = 0
